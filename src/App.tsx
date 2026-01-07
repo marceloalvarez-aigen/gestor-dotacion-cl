4d722914024a38c82a8ef45952ce071fa264596e
marceloalvarez-aigen <marceloalvarez.aigen@gmail.com>
2026-01-07 11:20:17 -0300

    Remove unused React import from App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
 import { AuthProvider } from './contexts/AuthContext';
 import ProtectedRoute from './components/ProtectedRoute';
